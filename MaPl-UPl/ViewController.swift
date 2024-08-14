a550503b325b29800659f819fb82fbd21197704a
yyeonjju <llilyha123@gmail.com>
2024-08-14 22:55:45 +0900

    Initial Commit

---
+++ MaPl-UPl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MaPl-UPl
+//
+//  Created by 하연주 on 8/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
